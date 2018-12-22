b7736198ab7620a3db84139ae120f68f630e8190
Mahesh G <gg.mahesh951@gmail.com>
2018-12-22 15:22:46 +0530

    logistic regr

---
+++ logistic_regression.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rc("font", size=14)
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+sns.set(style="white")
+#sns.set(style="whitegrid", color_codes=True)
+data = pd.read_csv(r'C:\Users\ggmah\Desktop\asdasd.txt', header=0)
+data = data.dropna()
+print(data.shape)
+print(list(data.columns))
+data['education']=np.where(data['education'] =='basic.9y', 'Basic', data['education'])
+data['education']=np.where(data['education'] =='basic.6y', 'Basic', data['education'])
+data['education']=np.where(data['education'] =='basic.4y', 'Basic', data['education'])
+sns.countplot(x='y',data=data, palette='hls')
+plt.show()
+data.groupby('job').mean()
+pd.crosstab(data.poutcome,data.y).plot(kind='bar')
+plt.title('Purchase Frequency for Poutcome')
+plt.xlabel('Poutcome')
+plt.ylabel('Frequency of Purchase')
+cat_vars = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', 'poutcome']
+for var in cat_vars:
+    cat_list = 'var' + '_' + var
+    cat_list = pd.get_dummies(data[var], prefix=var)
+    data1 = data.join(cat_list)
+    data = data1
+
+cat_vars = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', 'poutcome']
+data_vars = data.columns.values.tolist()
+to_keep = [i for i in data_vars if i not in cat_vars]
+data_final = data[to_keep]
+data_final.columns.values
+X = data_final.loc[:, data_final.columns != 'y']
+y = data_final.loc[:, data_final.columns == 'y']
+from imblearn.over_sampling import SMOTE
+
+os = SMOTE(random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+columns = X_train.columns
+
+os_data_X,os_data_y=os.fit_sample(X_train, y_train)
+os_data_X = pd.DataFrame(data=os_data_X,columns=columns )
+os_data_y= pd.DataFrame(data=os_data_y,columns=['y'])
+data_final_vars=data_final.columns.values.tolist()
+y=['y']
+X=[i for i in data_final_vars if i not in y]
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression()
+
+rfe = RFE(logreg, 20)
+rfe = rfe.fit(os_data_X, os_data_y.values.ravel())
+print(rfe.support_)
+print(rfe.ranking_)
+plt.show()
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
